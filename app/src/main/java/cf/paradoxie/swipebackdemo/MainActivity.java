c4d18d2838ad1ee955a85cdb5c48b865bf8cb62b
Paradox <xzq351107836@163.com>
2016-10-29 02:06:58 +0800

    初始化

---
+++ app/src/main/java/cf/paradoxie/swipebackdemo/MainActivity.java
@@ -0,0 +1,23 @@
+package cf.paradoxie.swipebackdemo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends BaseActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected boolean isSupportSwipeBack() {
+        return false;
+    }
+
+    public void nextPage(View v){
+        startActivity(new Intent(this,NextActivity.class));
+    }
+}
